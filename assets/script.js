7b803cc2b03beb53ef93ad30f630c8e1e5fb049f
AlexBouillaguet <alexandre.bouillaguet>
2024-07-10 18:07:36 +0200

    add the change of slide indefinitely

--- assets/script.js
+++ assets/script.js
@@ -16,19 +16,29 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
 const leftArrow = document.querySelector("#banner .arrow_left");
 const rightArrow = document.querySelector("#banner .arrow_right");
 const dots = document.querySelector("#banner .dots");
+const bannerImg = document.querySelector("#banner .banner-img");
+const bannerText = document.querySelector("#banner p");
+let currentIndex = 0;
 
 // écoute des événements pour les flèches
 
 leftArrow.addEventListener("click", () => {
-	console.log("leftArrow");
+	currentIndex--;
+	if (currentIndex = -1 ) {
+		currentIndex = slides.length - 1;
+	};
+	changeSlideAndDots(currentIndex);
 });
 
 rightArrow.addEventListener("click", () => {
-	console.log("rightArrow");
+	currentIndex++;
+	if (currentIndex === slides.length) {
+		currentIndex = 0
+	};
+	changeSlideAndDots(currentIndex);
 });
 
 // insertion des dots d'après le nombre de slides
@@ -37,10 +47,23 @@ for (let i = 0; i < slides.length; i++) {
 	const dot = document.createElement("div");
 	dot.classList.add("dot");
 	dots.appendChild(dot);
-}
+};	
 
 // ajout du dot sélectionné
 
 const allDots = document.querySelectorAll(".dot");
 allDots[0].classList.add("dot_selected");
 
+// fontion pour changer le slide et les dots
+
+function changeSlideAndDots (indexOfSlide) {
+	bannerImg.src = `./assets/images/slideshow/${slides[indexOfSlide].image}`;
+	bannerText.innerHTML = slides[indexOfSlide].tagLine;
+	allDots.forEach((dot, index) => {
+		if (index === currentIndex) {
+			dot.classList.add("dot_selected");
+		} else {
+			dot.classList.remove("dot_selected");
+		}	
+	});	
+};	
